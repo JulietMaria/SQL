7e2eb8f0d3ff4b11e0b755bc06a62a5b5a03196f
JJM <45478187+JulietMaria@users.noreply.github.com>
2023-02-21 07:26:08 +0530

    Create non-profitable-drugs.sql

---
+++ non-profitable-drugs.sql
@@ -0,0 +1,8 @@
+--https://datalemur.com/questions/non-profitable-drugs
+SELECT manufacturer,COUNT(drug) as drug_count,
+ABS(SUM(total_sales-cogs)) as total_loss
+FROM pharmacy_sales
+WHERE total_sales-cogs<=0
+GROUP BY manufacturer
+ORDER BY total_loss desc
+;
