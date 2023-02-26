eebf11d7a72c8727eb4b11b2272422a8b7c11dc6
JJM <45478187+JulietMaria@users.noreply.github.com>
2023-02-26 12:06:58 +0530

    Create time-spent-snaps.sql

---
+++ time-spent-snaps.sql
@@ -0,0 +1,37 @@
+--https://datalemur.com/questions/time-spent-snaps
+
+--Using CASE Statement & CTE[common table expression]
+WITH snaps_statistics AS (
+  SELECT 
+    age.age_bucket, 
+    SUM(CASE WHEN activities.activity_type = 'send' 
+      THEN activities.time_spent ELSE 0 END) AS send_timespent, 
+    SUM(CASE WHEN activities.activity_type = 'open' 
+      THEN activities.time_spent ELSE 0 END) AS open_timespent, 
+    SUM(activities.time_spent) AS total_timespent 
+  FROM activities
+ INNER JOIN age_breakdown AS age 
+    ON activities.user_id = age.user_id 
+  WHERE activities.activity_type IN ('send', 'open') 
+  GROUP BY age.age_bucket) 
+
+SELECT 
+  age_bucket, 
+  ROUND(100.0 * send_timespent / total_timespent, 2) AS send_perc, 
+  ROUND(100.0 * open_timespent / total_timespent, 2) AS open_perc 
+FROM snaps_statistics;
+
+--using FILTER
+SELECT 
+  age.age_bucket, 
+  ROUND(100.0 * 
+    SUM(activities.time_spent) FILTER (WHERE activities.activity_type = 'send')/
+      SUM(activities.time_spent),2) AS send_perc, 
+  ROUND(100.0 * 
+    SUM(activities.time_spent) FILTER (WHERE activities.activity_type = 'open')/
+      SUM(activities.time_spent),2) AS open_perc
+FROM activities
+INNER JOIN age_breakdown AS age 
+  ON activities.user_id = age.user_id 
+WHERE activities.activity_type IN ('send', 'open') 
+GROUP BY age.age_bucket;
