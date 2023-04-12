f9bde3ac60107a911ab7a440a6a51efee54577c8
JJM <45478187+JulietMaria@users.noreply.github.com>
2023-04-12 11:00:58 +0530

    Create game-play-analysis-iv.sql

---
+++ game-play-analysis-iv.sql
@@ -0,0 +1,8 @@
+--https://leetcode.com/problems/game-play-analysis-iv/description/
+
+
+SELECT ROUND(COUNT(t2.player_id)/COUNT(t1.player_id),2) AS fraction
+FROM
+(SELECT player_id, MIN(event_date) AS first_login FROM Activity GROUP BY player_id) t1 
+LEFT JOIN Activity t2
+ON t1.player_id = t2.player_id AND t1.first_login = t2.event_date - 1;
