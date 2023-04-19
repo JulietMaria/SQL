f704af857e1b02297a83fb620d0219f9dcb55d22
JJM <45478187+JulietMaria@users.noreply.github.com>
2023-04-19 19:48:30 +0530

    Create customer-placing-the-largest-number-of-orders.sql

---
+++ customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,7 @@
+--https://leetcode.com/problems/customer-placing-the-largest-number-of-orders/description/
+
+select customer_number 
+from Orders 
+group by customer_number 
+order by count(customer_number) 
+desc limit 1;
