d9ebc479ed9498e9653508db74600f5d92a0458f
JJM <45478187+JulietMaria@users.noreply.github.com>
2023-05-08 07:05:39 +0530

    Update 10026-find-all-wineries.sql

--- 10026-find-all-wineries.sql
+++ 10026-find-all-wineries.sql
@@ -4,6 +4,12 @@ select distinct winery
 FROM winemag_p1
 where lower(description) REGEXP '(plum|rose|cherry|hazelnut)([^a-z])';
 
+/*
+The query is filtering the results based on the contents of the "description" column. 
+Specifically, it is searching for any description that contains the words "plum", "rose", "cherry", or "hazelnut" 
+followed by a non-letter character (such as a space or punctuation mark). 
+[descriptions containing any of the specified words]
+*/
 
 --another solution
 select distinct winery
