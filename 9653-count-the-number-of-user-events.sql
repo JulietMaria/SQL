89d826945a5d14a2012bbc0db8e801ff417eb29f
JJM <45478187+JulietMaria@users.noreply.github.com>
2023-05-12 07:21:15 +0530

    Create 9653-count-the-number-of-user-events.sql

---
+++ 9653-count-the-number-of-user-events.sql
@@ -0,0 +1,7 @@
+--https://platform.stratascratch.com/coding/9653-count-the-number-of-user-events-performed-by-macbookpro-users?code_type=3
+
+select event_name,count(event_name) as event_count 
+from playbook_events
+where device='macbook pro'
+group by event_name
+order by event_count desc;
